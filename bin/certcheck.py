39a926c9714e0135a028def2ccaa2171dc10aa84
Pawel Rozlach <prozlach@spotify.com>
2013-09-16 20:18:53 +0200

    Implemented srv riemann detection
    Change-Id: Id366bc60cb7cdaa9e08c93e4db29edced8f073b4
    Reviewed-on: https://gerrit.spotify.net/gerrit/76386
    Reviewed-by: Paweł Rozlach <prozlach@spotify.com>

--- bin/certcheck.py
+++ bin/certcheck.py
@@ -14,13 +14,15 @@ from datetime import datetime, timedelta
 from dulwich.client import SSHGitClient, SubprocessWrapper, TraditionalGitClient
 from dulwich.protocol import Protocol
 from dulwich.repo import Repo
-from eagleeye.riemann import Riemann
 import argparse
+import bernhard
+import dns.resolver
 import fcntl
 import hashlib
 import logging
 import logging.handlers as lh
 import os
+import re
 import socket
 import subprocess
 import sys
@@ -204,12 +206,12 @@ class ScriptStatus(object):
     def _send_data(cls, event):
         for riemann_connection in cls._riemann_connections:
             logging.info('Sending event {0}, '.format(str(event)) +
-                         'using riemann conn {0}:{1}'.format(
+                         'using Riemann conn {0}:{1}'.format(
                              riemann_connection.host, riemann_connection.port)
                          )
             if not cls._debug:
                 try:
-                    riemann_connection.submit(event)
+                    riemann_connection.send(event)
                 except Exception as e:
                     logging.exception("Failed to send event to Rieman host: " +
                                       "{0}".format(str(e))
@@ -222,22 +224,107 @@ class ScriptStatus(object):
                              'real sent call')
 
     @classmethod
-    def initialize(cls, riemann_hosts, riemann_port, riemann_tags, debug=False):
+    def _name2ip(cls, name):
+        if re.match('\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}', name):
+            #IP entry:
+            return name
+        else:
+            #Hostname, we need to resolve it:
+            try:
+                ipaddr = dns.resolver.query(name, 'A')
+            except dns.resolver.NXDOMAIN:
+                logging.error("A record for {0} was not found".format(name))
+                return name  # Let somebody else worry about it ;)
+
+            return ipaddr[0].to_text()
+
+    @classmethod
+    def _resolve_srv_hosts(cls, name):
+        result = []
+        logging.debug("Resolving {0}, proto {1}")
+        if name.find('._udp') > 0:
+            proto = 'udp'
+        elif name.find('.tcp') > 0:
+            proto = 'tcp'
+        else:
+            raise RecoverableException("Entry {0}".format(name) +
+                                       "is not a valid SRV name")
+
+        try:
+            resolved = dns.resolver.query(name, 'SRV')
+        except dns.resolver.NXDOMAIN:
+            logging.error("Entry {0} does not exist, skipping.")
+            return []
+
+        for rdata in resolved:
+            entry = namedtuple("RiemannHost", ['host', 'port', 'proto'])
+            entry.host = cls._name2ip(rdata.target)
+            if entry.host is None:
+                continue
+            entry.port = rdata.port
+            entry.proto = proto
+            result.append(entry)
+            logging.debug("String {0} resolved as {1}".format(name, str(entry)))
+
+        return result
+
+    @classmethod
+    def _resolve_static_entry(cls, name):
+        entry = namedtuple("RiemannHost", ['host', 'port', 'proto'])
+        try:
+            a, b, c = name.split(":")
+            entry.host = cls._name2ip(a)
+            if entry.host is None:
+                raise ValueError()
+            entry.port = int(b)  # Raises ValueError by itself
+            if c in ['tcp', 'udp']:
+                entry.proto = c
+            else:
+                raise ValueError()
+        except ValueError:
+            logging.error("String {0} is not a valid ip:port:proto entry")
+            return []
+
+        logging.debug("String {0} resolved as {1}".format(name, str(entry)))
+        return [entry]
+
+    @classmethod
+    def initialize(cls, riemann_hosts_config, riemann_port, riemann_tags,
+                   debug=False):
         cls._riemann_tags = riemann_tags
         cls._hostname = socket.gethostname()
         cls._debug = debug
         cls._exit_status = 'ok'
         cls._exit_message = ''
-        cls._riemann_connections = []  # FIXME - We should probably do
+        cls._riemann_connections = []  # FIXME - we should probably do
                                        # some disconect here if we re-initialize
                                        # probably using conn.shutdown() call
 
         if not riemann_tags:
-            logging.error('There should be at least one riemann tag defined.')
-            return  # Should it sys.exit or just return ??
-        for riemann_host in riemann_hosts:
+            logging.error('there should be at least one Riemann tag defined.')
+            return  # should it sys.exit or just return ??
+        tmp = []
+        if "static" in riemann_hosts_config:
+            for line in riemann_hosts_config["static"]:
+                tmp.extend(cls._resolve_static_entry(line))
+
+        if "by_srv" in riemann_hosts_config:
+            for line in riemann_hosts_config["by_srv"]:
+                tmp.extend(cls._resolve_srv_hosts(line))
+
+        for riemann_host in tmp:
             try:
-                riemann_connection = Riemann(riemann_host, riemann_port)
+                if riemann_host.proto == 'tcp':
+                    riemann_connection = bernhard.Client(riemann_host.host,
+                                                         riemann_host.port,
+                                                         bernhard.TCPTransport)
+                elif riemann_host.proto == 'udp':
+                    riemann_connection = bernhard.Client(riemann_host.host,
+                                                         riemann_host.port,
+                                                         bernhard.UDPTransport)
+                else:
+                    logging.error("Unsupported transport {0}".format(riemann_host.proto) +
+                                  ", not connected to {1}".format(riemann_host))
             except Exception as e:
                 logging.exception("Failed to connect to Rieman host " +
                                   "{0}: {1}, ".format(riemann_host, str(e)) +
@@ -248,7 +335,7 @@ class ScriptStatus(object):
             cls._riemann_connections.append(riemann_connection)
 
         if not cls._riemann_connections:
-            logging.error("there are no active connections to Riemann, " +
+            logging.error("There are no active connections to Riemann, " +
                           "metrics will not be send!")
 
     @classmethod
@@ -482,8 +569,7 @@ def main(config_file, std_err=False, verbose=True, dont_send=False):
 
         #Initialize Riemann reporting:
         ScriptStatus.initialize(
-            riemann_hosts=ScriptConfiguration.get_val("riemann_hosts"),
-            riemann_port=ScriptConfiguration.get_val("riemann_port"),
+            riemann_hosts_config=ScriptConfiguration.get_val("riemann_hosts_config"),
             riemann_tags=ScriptConfiguration.get_val("riemann_tags"),
             debug=dont_send,
         )
